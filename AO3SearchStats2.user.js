a30ab6c27dc75e2d6a44a39d45b7e7de2ed7cb8b
Amber Alex <13611718+mxamber@users.noreply.github.com>
2021-11-18 21:35:49 +0100

    Bug fixes, better sanitising of values
    v2.0.1: script no longer crashes if hits or kudos are not displayed due to being zero. added "okay" tier to chapter word count. rounding kudos:hits ratio to one decimal instead of two.

--- AO3SearchStats2.user.js
+++ AO3SearchStats2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AO3SearchStats2
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.0.1
 // @description  Additional information in AO3 search results
 // @author       mxamber
 // @match        https://archiveofourown.org/*
@@ -33,39 +33,75 @@
     let THRESHOLD_BAD_FICS = 2.5;
     let THRESHOLD_OKAY_FICS = 3.5;
     let THRESHOLD_GOOD_FICS = 8.5;
-    let THRESHOLD_WORD_COUNT = 2000;
+    let THRESHOLD_WORDS_OKAY = 2000;
+    let THRESHOLD_WORDS_BAD = 1250;
     let THRESHOLD_MIN_HITS = 1000;
+
+
+    // define colours once and use variables throughout the rest
+
+    let RED = "#990000";
+    let YELLOW = "#e0b60b";
+    let GREEN = "#38ad2d";
+    let CYAN = "#0bde98";
 
     let fics = document.querySelectorAll("dl.stats");
     fics.forEach(element => {
+        let title = element.parentNode.querySelector(".header h4 a").innerText;
+        console.log(`[AO3SeachStatis2] now processing: "${title}"`);
+
+        // fetch word count. if for some reason NaN, substitute 0
         let words = parseInt(element.querySelector("dd.words").innerText.replace(",", ""));
-        let chapters_temp = element.querySelector("dd.chapters");
-        let kudos = parseInt(element.querySelector("dd.kudos").innerText);
-        let hits = parseInt(element.querySelector("dd.hits").innerText);
-
         words = isNaN(words) ? 0 : words;
-        kudos = isNaN(kudos) ? 0 : kudos;
-        hits = isNaN(hits) ? 0 : hits;
 
+        // fetch amount of chapters (format e.g. "1/13 chapters") and extract number using regex
+        // sanitise like words (re: NaN)
+        let chapters_temp = element.querySelector("dd.chapters");
         let chapter_regex = /(\d+)\/(?:\?|\d+)/;
         let chapters = parseInt(chapter_regex.exec(chapters_temp.innerText)[1]);
         chapters = isNaN(chapters) ? 0 : chapters;
 
-        // console.log(`${words} words, ${chapters} chapters, ${comments} comments, ${kudos} kudos, ${bookmarks} bookmarks, ${hits} hits`);
-
+        // do the maths. round to whole numbers, one word more or less doesn't matter
         let words_per_chapter = Math.round(words / chapters);
-        let kudos_percents = kudos / hits * 100;
-        kudos_percents = Math.round(kudos_percents * 100) / 100;
 
+        // display as tooltip and change colour accordingly
         let elem_chapters = element.querySelector("dt.chapters");
         elem_chapters.title = `${words_per_chapter} words per chapter`;
         elem_chapters.style.fontWeight = "bold";
-        elem_chapters.style.color = words_per_chapter < THRESHOLD_WORD_COUNT ? "#900" : "#38ad2d";
+        elem_chapters.style.color = words_per_chapter > THRESHOLD_WORDS_OKAY ? GREEN : words_per_chapter > THRESHOLD_WORDS_BAD ? YELLOW : RED;
 
+        /*
+        kudos and hits are a little more complicated, since both might be 0
+        if kudos are 0, the element won't even exist (not sure about hits)
+        if the element doesn't exist, then calculating the ratio is pointless, as it can't be displayed
+        in that case, abort right away (since forEach runs a function, rather than a loop, it's "return", not "continue")
+        */
+        if(
+            element.querySelector("dt.kudos") == undefined ||
+            element.querySelector("dd.kudos") == undefined ||
+            element.querySelector("dt.hits") == undefined ||
+            element.querySelector("dd.hits") == undefined
+        ) {
+            return;
+        }
+
+        // element to later display the ratio on
         let elem_kudos = element.querySelector("dt.kudos");
+
+        // fetch kudos and hits. sanitise like usual
+        let kudos = parseInt(element.querySelector("dd.kudos").innerText);
+        let hits = parseInt(element.querySelector("dd.hits").innerText);
+        kudos = isNaN(kudos) ? 0 : kudos;
+        hits = isNaN(hits) ? 0 : hits;
+
+        // calculate kudos and round to one decimal digit
+        let kudos_percents = kudos / hits * 100;
+        kudos_percents = Math.round(kudos_percents * 10) / 10;
+
+        // display as tooltip and change color accordingly
         elem_kudos.title = `${kudos_percents}%  of hits left kudos`;
         elem_kudos.style.fontWeight = "bold";
-        elem_kudos.style.color = kudos_percents < THRESHOLD_BAD_FICS ? "#900" : kudos_percents < THRESHOLD_OKAY_FICS ? "#e0b60b" : kudos_percents > THRESHOLD_GOOD_FICS && hits > THRESHOLD_MIN_HITS ? "#0bde98" : "#38ad2d";
+        elem_kudos.style.color = kudos_percents < THRESHOLD_BAD_FICS ? RED : kudos_percents < THRESHOLD_OKAY_FICS ? YELLOW : kudos_percents > THRESHOLD_GOOD_FICS && hits > THRESHOLD_MIN_HITS ? CYAN : GREEN;
 
     });
 })();
